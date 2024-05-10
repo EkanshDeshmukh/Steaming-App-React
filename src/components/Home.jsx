5a622527b3bfa1fedfef2a2b18b320f605c09c80
EkanshDeshmukh <yashudeshmukh25@gmail.com>
2024-05-10 22:19:24 +0530

    Basic setup Done

---
+++ src/components/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Sidenav from '../partials/Sidenav'
+
+const Home = () => {
+    return (
+        <div className='flex w-full h-full'>
+            <Sidenav />
+        </div>
+    )
+}
+
+export default Home
