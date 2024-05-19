5919b13a366d78fef561b65ce27f9be4b07f4386
EkanshDeshmukh <yashudeshmukh25@gmail.com>
2024-05-20 00:38:56 +0530

    Added People card

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import Trending from './components/Trending'
 import Popular from './components/Popular'
 import Movies from './components/Movies'
 import TvShow from './components/TvShow'
+import People from './components/People'
 
 const App = () => {
   return (
@@ -15,6 +16,7 @@ const App = () => {
         <Route path='/popular' element={<Popular />}></Route>
         <Route path='/movie' element={<Movies />}></Route>
         <Route path='/tv' element={<TvShow />}></Route>
+        <Route path='/person' element={<People />}></Route>
       </Routes>
     </div>
   )
